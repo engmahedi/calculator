5b918b9623f5edd7193789d23e8929e5804938e2
Mahedi <faruk585@gmail.com>
2023-08-09 15:12:52 +0600

    if else complete

---
+++ calculator.js
@@ -0,0 +1,82 @@
+
+
+// Make calculator for Two Number using "if else" .
+
+ function calculator(operator,num1,num2) {
+  
+  if (operator=='+') {
+    result = num1 + num2;
+    
+  } else if (operator=='-') {
+    result = num1 - num2;
+    
+  }else if (operator=='*') {
+    result = num1 * num2;
+    
+  }else if (operator=='/') {
+    result = num1 / num2;
+    
+  }else if(operator=='%') {
+    result = num1 % num2;
+    
+  } else {
+    result= "Invalid operation";
+  }
+  
+  return result
+
+}
+
+console.log('Addition : '+ (calculator( '+', 20,15)));
+console.log('Subtraction : '+ (calculator( '-', 20,15)));
+console.log('Multiplication : '+ (calculator( '*', 20,15)));
+console.log('Division : '+ (calculator( '/', 20,15)));
+console.log('Modulus : '+ (calculator( '%', 23,2)));
+
+
+
+// Make calculator for Two Number using "switch" .
+/*
+function calculator(operator,num1, num2) {
+  
+
+  switch (operator) {
+
+    case ('+'):
+      result = num1 + num2;
+      break;
+    
+      case ('-'):
+        result = num1 - num2;
+      break;
+    
+      case ('*'):
+        result = num1 * num2;
+      break;
+    
+      case ('/'):
+        result = num1 / num2;
+      break;
+    
+      case ('%'):
+        result = num1 % num2;
+        break;
+    
+    default:
+      result = 'Invalid operation';
+      break;
+  }
+
+  return result;
+}
+
+
+
+console.log('Addition : '+ (calculator( '+', 10,15)));
+console.log('Subtraction : '+ (calculator( '-', 10,15)));
+console.log('Multiplication : '+ (calculator( '*', 10,15)));
+console.log('Division : '+ (calculator( '/', 10,15)));
+console.log('Modulus : '+ (calculator( '%', 13,2)));
+
+
+*/
